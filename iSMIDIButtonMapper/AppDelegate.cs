6e32e9b28ffae095340b6a1c638d88f16685204e
Nigel Brown <nigel@impromptu-software.co.uk>
2021-01-24 11:10:00 +0000

    * .gitignore: Initial commit
    - Any incoming MIDI event on the default MIDI device will trigger
      Ctrl+Shift+Command+F12
    
    * iSMIDIButtonMapper.sln:
    * Main.cs:
    * Info.plist:
    * Structs.cs:
    * AppDelegate.cs:
    * packages.config:
    * Main.storyboard:
    * ViewController.cs:
    * Entitlements.plist:
    * ApiDefinition.cs:
    * iSMIDIButtonMapper.csproj:
    * ViewController.designer.cs:
    * AssemblyInfo.cs:
    * Contents.json:
    * NativeKeyboardEvents.csproj:
    * Contents.json:
    * AppIcon-16.png:
    * AppIcon-32.png:
    * AppIcon-256.png:
    * AppIcon-512.png:
    * AppIcon-128.png:
    * AppIcon-32@2x.png:
    * AppIcon-16@2x.png:
    * AppIcon-256@2x.png:
    * AppIcon-512@2x.png:
    * AppIcon-128@2x.png:

---
+++ iSMIDIButtonMapper/AppDelegate.cs
@@ -0,0 +1,65 @@
+﻿using AppKit;
+using Foundation;
+using Commons.Music.Midi;
+using System;
+using System.Linq;
+using CoreGraphics;
+
+namespace iSMIDIButtonMapper
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : NSApplicationDelegate
+    {
+        public AppDelegate()
+        {
+        }
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            // Insert code here to initialize your application
+
+            // Enumerate the MIDI devices available
+
+            var access = MidiAccessManager.Default;
+            foreach (var i in access.Inputs)
+                Console.WriteLine(i.Id + " : " + i.Name);
+            if (!access.Inputs.Any())
+            {
+                Console.WriteLine("No input device found.");
+                return;
+            }
+            var iport = access.Inputs.FirstOrDefault(i => i.Id == "Arduino Leonardo") ?? access.Inputs.Last();
+            var input = access.OpenInputAsync(iport.Id).Result;
+            Console.WriteLine("Using " + iport.Id);
+            input.MessageReceived += (obj, e) => {
+                Console.WriteLine($"{e.Timestamp} {e.Start} {e.Length} {e.Data[0].ToString("X")}");
+
+
+                InvokeOnMainThread(() => // Need to run the keyboard interaction code on the main thread
+                {
+                    using (var eventSource = new CGEventSource(CGEventSourceStateID.CombinedSession))
+                    {
+                        var key = (ushort)NSKey.F12;
+                        if (e.Data[0] == 0x90)
+                        {
+                            var keyDownEvent = new CGEvent(eventSource, key, true);
+                            keyDownEvent.Flags = CGEventFlags.Control | CGEventFlags.Shift | CGEventFlags.Command;
+                            CGEvent.Post(keyDownEvent, CGEventTapLocation.AnnotatedSession);
+
+                            var keyUpEvent = new CGEvent(eventSource, key, false);
+                            keyUpEvent.Flags = CGEventFlags.Control | CGEventFlags.Shift | CGEventFlags.Command;
+                            CGEvent.Post(keyUpEvent, CGEventTapLocation.AnnotatedSession);
+
+                        }
+                    };
+                });
+            };
+
+        }
+
+        public override void WillTerminate(NSNotification notification)
+        {
+            // Insert code here to tear down your application
+        }
+    }
+}
