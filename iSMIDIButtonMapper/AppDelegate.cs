bffc4ab2b1083dffd9e2e213324cca3a67be72e1
Nigel Brown <nigel@impromptu-software.co.uk>
2021-01-24 14:04:23 +0000

    Add a map for translation between MIDI key values and Virtual key codes
    - Map uses notes 60,61 and 62 and translates through to Ctrl,Shift,Cmd F10,F11,F12

--- iSMIDIButtonMapper/AppDelegate.cs
+++ iSMIDIButtonMapper/AppDelegate.cs
@@ -4,6 +4,7 @@ using Commons.Music.Midi;
 using System;
 using System.Linq;
 using CoreGraphics;
+using System.Collections.Generic;
 
 namespace iSMIDIButtonMapper
 {
@@ -12,6 +13,17 @@ namespace iSMIDIButtonMapper
     {
         public AppDelegate()
         {
+        }
+
+        public struct KeyboardEvent
+        {
+            public KeyboardEvent(ushort iKeyValue, CGEventFlags iModifierKeys)
+            {
+                keyValue = iKeyValue;
+                modifierKeys = iModifierKeys;
+            }
+            public ushort keyValue;
+            public CGEventFlags modifierKeys;
         }
 
         public override void DidFinishLaunching(NSNotification notification)
@@ -28,31 +40,85 @@ namespace iSMIDIButtonMapper
                 Console.WriteLine("No input device found.");
                 return;
             }
+
+            var noteToKeyboardMap = new Dictionary<byte, KeyboardEvent>();
+            noteToKeyboardMap.Add(60, new KeyboardEvent((ushort)NSKey.F10, CGEventFlags.Control | CGEventFlags.Shift | CGEventFlags.Command));
+            noteToKeyboardMap.Add(61, new KeyboardEvent((ushort)NSKey.F11, CGEventFlags.Control | CGEventFlags.Shift | CGEventFlags.Command));
+            noteToKeyboardMap.Add(62, new KeyboardEvent((ushort)NSKey.F12, CGEventFlags.Control | CGEventFlags.Shift | CGEventFlags.Command));
+
             var iport = access.Inputs.FirstOrDefault(i => i.Id == "Arduino Leonardo") ?? access.Inputs.Last();
             var input = access.OpenInputAsync(iport.Id).Result;
             Console.WriteLine("Using " + iport.Id);
             input.MessageReceived += (obj, e) => {
                 Console.WriteLine($"{e.Timestamp} {e.Start} {e.Length} {e.Data[0].ToString("X")}");
 
+                bool noteEvent = false;
+                bool noteOn = false;
+                byte channel = 0;
+                byte midiKey = 0;
+                byte velocity = 0;
 
-                InvokeOnMainThread(() => // Need to run the keyboard interaction code on the main thread
+                // Decode the MIDI packet for note information
+                byte startByte = e.Data[0];
+                if ((startByte & 0x90) == 0x90)
                 {
-                    using (var eventSource = new CGEventSource(CGEventSourceStateID.CombinedSession))
+                    // We have a Note On event
+                    noteEvent = true;
+                    noteOn = true;
+
+                    // Decode the channel
+                    channel = (byte)(startByte & 0x0F);
+
+                    // Decode the key value
+                    midiKey = (byte)(e.Data[1] & 0x7F);
+
+                    // Decode the velocity
+                    velocity = (byte)(e.Data[2] & 0x7F);
+                }
+                else if ((startByte & 0x80) == 0x80)
+                {
+                    // We have a Note Off event
+                    noteEvent = true;
+                    noteOn = false;
+
+                    // Decode the channel
+                    channel = (byte)(startByte & 0x0F);
+
+                    // Decode the key value
+                    midiKey = (byte)(e.Data[1] & 0x7F);
+
+                    // Decode the velocity
+                    velocity = (byte)(e.Data[2] & 0x7F);
+                }
+
+                if (noteEvent == true)
+                {
+                    // Calculate keyboard event to send based on MIDI key
+                    var key = noteToKeyboardMap[midiKey].keyValue;
+                    CGEventFlags mods = noteToKeyboardMap[midiKey].modifierKeys;
+
+
+                    InvokeOnMainThread(() => // Need to run the keyboard interaction code on the main thread
                     {
-                        var key = (ushort)NSKey.F12;
-                        if (e.Data[0] == 0x90)
+                        using (var eventSource = new CGEventSource(CGEventSourceStateID.CombinedSession))
                         {
-                            var keyDownEvent = new CGEvent(eventSource, key, true);
-                            keyDownEvent.Flags = CGEventFlags.Control | CGEventFlags.Shift | CGEventFlags.Command;
-                            CGEvent.Post(keyDownEvent, CGEventTapLocation.AnnotatedSession);
+                            if (noteOn)
+                            {
+                                var keyDownEvent = new CGEvent(eventSource, key, true);
+                                keyDownEvent.Flags = mods;
+                                CGEvent.Post(keyDownEvent, CGEventTapLocation.AnnotatedSession);
+                            }
+                            else
+                            {
+                                var keyUpEvent = new CGEvent(eventSource, key, false);
+                                keyUpEvent.Flags = mods;
+                                CGEvent.Post(keyUpEvent, CGEventTapLocation.AnnotatedSession);
+                            }
+                        };
+                    });
 
-                            var keyUpEvent = new CGEvent(eventSource, key, false);
-                            keyUpEvent.Flags = CGEventFlags.Control | CGEventFlags.Shift | CGEventFlags.Command;
-                            CGEvent.Post(keyUpEvent, CGEventTapLocation.AnnotatedSession);
+                }
 
-                        }
-                    };
-                });
             };
 
         }
