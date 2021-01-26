08486f1a7db0d52b43ce5c128030fea467199c75
Nigel James Brown <nigel@impromptu-software.co.uk>
2021-01-26 22:10:22 +0000

    Moves code and extends for sending MIDI back out
    - Code moved from Form1.Designer.cs into Form1.cs where it should be
    - Three on-screen buttons added that, when pressed, send MIDI note events out to the Arduino MIDI device

--- iSMIDIButtonMapperWinFormsDOTNET/Form1.Designer.cs
+++ iSMIDIButtonMapperWinFormsDOTNET/Form1.Designer.cs
@@ -1,8 +1,4 @@
-﻿using Commons.Music.Midi;
-using System;
-using System.Linq;
-using System.Collections.Generic;
-using System.Runtime.InteropServices;
+﻿
 
 namespace iSMIDIButtonMapperWinFormsDOTNET
 {
@@ -26,29 +22,6 @@ namespace iSMIDIButtonMapperWinFormsDOTNET
             base.Dispose(disposing);
         }
 
-        public struct KeyboardEvent
-        {
-            public KeyboardEvent(ushort iKeyValue, UInt32 iModifierKeys)
-            {
-                keyValue = iKeyValue;
-                modifierKeys = iModifierKeys;
-            }
-            public ushort keyValue;
-            public UInt32 modifierKeys;
-        }
-
-        // Import the user32.dll
-        [DllImport("user32.dll", SetLastError = true)]
-        static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
-        // Declare some keyboard keys as constants with its respective code
-        // See Virtual Code Keys: https://msdn.microsoft.com/en-us/library/dd375731(v=vs.85).aspx
-        public const int KEYEVENTF_EXTENDEDKEY = 0x0001; //Key down flag
-        public const int KEYEVENTF_KEYUP = 0x0002; //Key up flag
-        public const int VK_RCONTROL = 0xA3; //Right Control key code
-        public const int VK_F10 = 0x79;
-        public const int VK_F11 = 0x7A;
-        public const int VK_F12 = 0x7B;
-
 
         #region Windows Form Designer generated code
 
@@ -58,100 +31,67 @@ namespace iSMIDIButtonMapperWinFormsDOTNET
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(233, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 72);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Lime;
+            this.button2.Location = new System.Drawing.Point(137, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 72);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button3.Location = new System.Drawing.Point(39, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 72);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(376, 96);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
-
-
-            // Enumerate the MIDI devices available
-
-            var access = MidiAccessManager.Default;
-            foreach (var i in access.Inputs)
-                Console.WriteLine(i.Id + " : " + i.Name);
-            if (!access.Inputs.Any())
-            {
-                Console.WriteLine("No input device found.");
-                return;
-            }
-
-            var noteToKeyboardMap = new Dictionary<byte, KeyboardEvent>();
-            noteToKeyboardMap.Add(60, new KeyboardEvent((ushort)VK_F10, 0));
-            noteToKeyboardMap.Add(61, new KeyboardEvent((ushort)VK_F11, 0));
-            noteToKeyboardMap.Add(62, new KeyboardEvent((ushort)VK_F12, 0));
-
-            var iport = access.Inputs.FirstOrDefault(i => i.Id == "Arduino Leonardo") ?? access.Inputs.Last();
-            var input = access.OpenInputAsync(iport.Id).Result;
-            Console.WriteLine("Using " + iport.Id);
-            input.MessageReceived += (obj, e) =>
-            {
-                Console.WriteLine($"{e.Timestamp} {e.Start} {e.Length} {e.Data[0].ToString("X")}");
-
-                bool noteEvent = false;
-                bool noteOn = false;
-                byte channel = 0;
-                byte midiKey = 0;
-                byte velocity = 0;
-
-                // Decode the MIDI packet for note information
-                byte startByte = e.Data[0];
-                if ((startByte & 0x90) == 0x90)
-                {
-                    // We have a Note On event
-                    noteEvent = true;
-                    noteOn = true;
-
-                    // Decode the channel
-                    channel = (byte)(startByte & 0x0F);
-
-                    // Decode the key value
-                    midiKey = (byte)(e.Data[1] & 0x7F);
-
-                    // Decode the velocity
-                    velocity = (byte)(e.Data[2] & 0x7F);
-                }
-                else if ((startByte & 0x80) == 0x80)
-                {
-                    // We have a Note Off event
-                    noteEvent = true;
-                    noteOn = false;
-
-                    // Decode the channel
-                    channel = (byte)(startByte & 0x0F);
-
-                    // Decode the key value
-                    midiKey = (byte)(e.Data[1] & 0x7F);
-
-                    // Decode the velocity
-                    velocity = (byte)(e.Data[2] & 0x7F);
-                }
-
-                if (noteEvent == true)
-                {
-                    // Calculate keyboard event to send based on MIDI key
-                    var key = noteToKeyboardMap[midiKey].keyValue;
-                    UInt32 mods = noteToKeyboardMap[midiKey].modifierKeys;
-
-
-                    if (noteOn)
-                    {
-                        // Simulate a key press event
-                        keybd_event((byte)key, 0, KEYEVENTF_EXTENDEDKEY, 0);
-                    }
-                    else
-                    {
-                        // Simulate a key release event
-                        keybd_event((byte)key, 0, KEYEVENTF_KEYUP, 0);
-
-                    }
-
-                }
-
-            };
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
+
+
+// TODO: https://www.c-sharpcorner.com/UploadFile/f9f215/how-to-minimize-your-application-to-system-tray-in-C-Sharp/
 
--- iSMIDIButtonMapperWinFormsDOTNET/Form1.cs
+++ iSMIDIButtonMapperWinFormsDOTNET/Form1.cs
@@ -8,6 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using Commons.Music.Midi;
+using System.Runtime.InteropServices;
+
 namespace iSMIDIButtonMapperWinFormsDOTNET
 {
     public partial class Form1 : Form
@@ -15,6 +18,193 @@ namespace iSMIDIButtonMapperWinFormsDOTNET
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public struct KeyboardEvent
+        {
+            public KeyboardEvent(ushort iKeyValue, UInt32 iModifierKeys)
+            {
+                keyValue = iKeyValue;
+                modifierKeys = iModifierKeys;
+            }
+            public ushort keyValue;
+            public UInt32 modifierKeys;
+        }
+
+        // Import the user32.dll
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+        // Declare some keyboard keys as constants with its respective code
+        // See Virtual Code Keys: https://msdn.microsoft.com/en-us/library/dd375731(v=vs.85).aspx
+        public const int KEYEVENTF_EXTENDEDKEY = 0x0001; //Key down flag
+        public const int KEYEVENTF_KEYUP = 0x0002; //Key up flag
+        public const int VK_RCONTROL = 0xA3; //Right Control key code
+
+        public const int VK_F1 = 0x70;
+        public const int VK_F2 = 0x71;
+        public const int VK_F3 = 0x72;
+        public const int VK_F4 = 0x73;
+        public const int VK_F5 = 0x74;
+        public const int VK_F6 = 0x75;
+        public const int VK_F7 = 0x76;
+        public const int VK_F8 = 0x77;
+        public const int VK_F9 = 0x78;
+        public const int VK_F10 = 0x79;
+        public const int VK_F11 = 0x7A;
+        public const int VK_F12 = 0x7B;
+        public const int VK_F13 = 0x7C;
+        public const int VK_F14 = 0x7D;
+        public const int VK_F15 = 0x7E;
+        public const int VK_F16 = 0x7F;
+        public const int VK_F17 = 0x80;
+        public const int VK_F18 = 0x81;
+        public const int VK_F19 = 0x82;
+        public const int VK_F20 = 0x83;
+        public const int VK_F21 = 0x84;
+        public const int VK_F22 = 0x85;
+        public const int VK_F23 = 0x86;
+        public const int VK_F24 = 0x87;
+
+        private void InitialiseMIDI()
+        {
+
+            // Enumerate the MIDI devices available
+
+            var access = MidiAccessManager.Default;
+            foreach (var i in access.Inputs)
+                Console.WriteLine(i.Id + " : " + i.Name);
+            if (!access.Inputs.Any())
+            {
+                Console.WriteLine("No input device found.");
+                return;
+            }
+
+            var noteToKeyboardMap = new Dictionary<byte, KeyboardEvent>();
+            noteToKeyboardMap.Add(60, new KeyboardEvent((ushort)VK_F18, 0));
+            noteToKeyboardMap.Add(61, new KeyboardEvent((ushort)VK_F19, 0));
+            noteToKeyboardMap.Add(62, new KeyboardEvent((ushort)VK_F20, 0));
+
+            var iport = access.Inputs.FirstOrDefault(i => i.Id == "Arduino Leonardo") ?? access.Inputs.Last();
+            var input = access.OpenInputAsync(iport.Id).Result;
+            Console.WriteLine("Using " + iport.Id);
+            input.MessageReceived += (obj, e) =>
+            {
+                Console.WriteLine($"{e.Timestamp} {e.Start} {e.Length} {e.Data[0].ToString("X")}");
+
+                bool noteEvent = false;
+                bool noteOn = false;
+                byte channel = 0;
+                byte midiKey = 0;
+                byte velocity = 0;
+
+                // Decode the MIDI packet for note information
+                byte startByte = e.Data[0];
+                if ((startByte & 0x90) == 0x90)
+                {
+                    // We have a Note On event
+                    noteEvent = true;
+                    noteOn = true;
+
+                    // Decode the channel
+                    channel = (byte)(startByte & 0x0F);
+
+                    // Decode the key value
+                    midiKey = (byte)(e.Data[1] & 0x7F);
+
+                    // Decode the velocity
+                    velocity = (byte)(e.Data[2] & 0x7F);
+                }
+                else if ((startByte & 0x80) == 0x80)
+                {
+                    // We have a Note Off event
+                    noteEvent = true;
+                    noteOn = false;
+
+                    // Decode the channel
+                    channel = (byte)(startByte & 0x0F);
+
+                    // Decode the key value
+                    midiKey = (byte)(e.Data[1] & 0x7F);
+
+                    // Decode the velocity
+                    velocity = (byte)(e.Data[2] & 0x7F);
+                }
+
+                if (noteEvent == true)
+                {
+                    // Calculate keyboard event to send based on MIDI key
+                    var key = noteToKeyboardMap[midiKey].keyValue;
+                    UInt32 mods = noteToKeyboardMap[midiKey].modifierKeys;
+
+
+                    if (noteOn)
+                    {
+                        // Simulate a key press event
+                        keybd_event((byte)key, 0, KEYEVENTF_EXTENDEDKEY, 0);
+
+                        Invoke((MethodInvoker)delegate
+                        {
+                            if (midiKey == 60)
+                            {
+                                this.button1.Select();
+                            }
+                            else if (midiKey == 61)
+                            {
+                                this.button2.Select();
+                            }
+                            else if (midiKey == 62)
+                            {
+                                this.button3.Select();
+                            }
+                        }); 
+                    }
+                    else
+                    {
+                        // Simulate a key release event
+                        keybd_event((byte)key, 0, KEYEVENTF_KEYUP, 0);
+
+                    }
+
+                }
+
+            };
+
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitialiseMIDI();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var access = MidiAccessManager.Default;
+            var iport = access.Outputs.FirstOrDefault(i => i.Id == "Arduino Leonardo") ?? access.Outputs.Last();
+            var output = access.OpenOutputAsync(iport.Id).Result;
+            output.Send(new byte[] { MidiEvent.NoteOn, 60, 0x70 }, 0, 3, 0);
+            output.CloseAsync();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var access = MidiAccessManager.Default;
+            var iport = access.Outputs.FirstOrDefault(i => i.Id == "Arduino Leonardo") ?? access.Outputs.Last();
+            var output = access.OpenOutputAsync(iport.Id).Result;
+            output.Send(new byte[] { MidiEvent.NoteOn, 61, 0x70 }, 0, 3, 0);
+            output.CloseAsync();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var access = MidiAccessManager.Default;
+            var iport = access.Outputs.FirstOrDefault(i => i.Id == "Arduino Leonardo") ?? access.Outputs.Last();
+            var output = access.OpenOutputAsync(iport.Id).Result;
+            output.Send(new byte[] { MidiEvent.NoteOn, 62, 0x70 }, 0, 3, 0);
+            output.CloseAsync();
+
         }
     }
 }
